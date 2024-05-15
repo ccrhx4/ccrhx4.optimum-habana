811fada9eb4c65a0c6e7b159516b843c843cc590
ccrhx4 <huanxing.shen@intel.com>
2024-05-15 02:04:55 +0000

    update hpu result

--- examples/language-modeling/run_mixtral_8_card.sh
+++ examples/language-modeling/run_mixtral_8_card.sh
@@ -8,24 +8,24 @@ python ../gaudi_spawn.py --use_deepspeed --world_size $num_cards run_lora_clm.py
 	--bf16 True \
 	--dataset_name tatsu-lab/alpaca \
 	--dataset_concatenation \
-	--per_device_train_batch_size 16 \
+	--per_device_train_batch_size 12 \
     	--per_device_eval_batch_size 8 \
     	--gradient_accumulation_steps 1 \
 	--gradient_checkpointing \
-	--max_seq_len 512 \
+	--max_seq_len 2048 \
     	--do_train \
      	--learning_rate 1e-4 \
 	--logging_steps 1 \
 	--overwrite_output_dir \
 	--log_level info \
 	--output_dir ./mixtral_peft_finetuned_model \
-	--peft lora \
+	--peft_type lora \
 	--lora_target_modules q_proj v_proj \
      	--lora_rank 4 \
 	--use_habana \
 	--use_lazy_mode \
 	--use_cache False \
 	--throughput_warmup_steps 3 \
-	--max_steps 5 \
+	--save_strategy "no" \
 	--deepspeed llama2_ds_zero3_config.json
 
